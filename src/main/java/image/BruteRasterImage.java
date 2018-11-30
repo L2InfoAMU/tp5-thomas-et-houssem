3e331fc7f9412aced53e700e80cbcfa176854005
n17019061 <thomas.ninci@etu.univ-amu.fr>
2018-11-30 09:59:41 +0100

    modification des constructeurs de BruteRasterImage

--- src/main/java/image/BruteRasterImage.java
+++ src/main/java/image/BruteRasterImage.java
@@ -4,27 +4,18 @@ import javafx.scene.paint.Color;
 
 public class BruteRasterImage implements Image
 {
-        private Color color;
+        private Color[][] pixels;
         public int width;
         public int height;
 
         public BruteRasterImage(Color color, int width, int height)
         {
-            this.color=color;
+            this.pixels= Color[width][height] pixels;
             this.width=width;
             this.height=height;
         }
         public BruteRasterImage(Color[][] colors)
         {
-            public void createRepresentation();
-            int i,j;
-            for(i=0; i< getWidth();i++)
-            {
-                for(j=0;j< getHeight();j++)
-                {
-                    public void setPixelColor(Color color, int x, int y);
-                    public Color getPixelColor(int x, int y);
-                }
-            }
+            this.pixels= colors;
         }
 }
