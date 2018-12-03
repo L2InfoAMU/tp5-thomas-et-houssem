dcc46aed01114306e2c2a8e48a0af37f14939aa4
n17019061 <thomas.ninci@etu.univ-amu.fr>
2018-12-03 15:54:04 +0100

    BruteRasterImage

--- src/main/java/image/BruteRasterImage.java
+++ src/main/java/image/BruteRasterImage.java
@@ -2,20 +2,39 @@ package image;
 
 import javafx.scene.paint.Color;
 
-public class BruteRasterImage implements Image
-{
-        private Color[][] pixels;
-        public int width;
-        public int height;
+public class BruteRasterImage implements Image {
+    private Color[][] pixels;
+    public int width;
+    public int height;
 
-        public BruteRasterImage(Color color, int width, int height)
-        {
-            this.pixels= Color[width][height] pixels;
-            this.width=width;
-            this.height=height;
+    @Override
+    public Color getPixelColor(int x, int y) {
+        return null;
+    }
+
+    @Override
+    public int getWidth() {
+        return 0;
+    }
+
+    @Override
+    public int getHeight() {
+        return 0;
+    }
+
+    public BruteRasterImage(Color color, int width, int height) {
+        this.pixels = new Color[width][height];
+        this.width = width;
+        this.height = height;
+        for (width = 0; width < getWidth(); width++) {
+            Color[width][]=color;
+            for (height = 0; height < getHeight(); height++) {
+                Color[][height]=color;
+            }
         }
-        public BruteRasterImage(Color[][] colors)
-        {
-            this.pixels= colors;
-        }
+    }
+
+    public BruteRasterImage(Color[][] colors) {
+        this.pixels = colors;
+    }
 }
